5e91e604adf8846cc087e5d3311b7329291b9276
Codeslip <jason@upmarket.ai>
2024-02-27 02:34:17 -0500

    working up to search

--- src/sechallenge/api.py
+++ src/sechallenge/api.py
@@ -1,6 +1,8 @@
 import logging
-import fitz #
+logging.basicConfig(level=logging.ERROR)
+logging.error('This will get logged')
 import os
+import fitz
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,190 +15,126 @@ from typing import Annotated
 from fastapi import FastAPI, Query
 from fastapi.staticfiles import StaticFiles
 
-from sentence_transformers import SentenceTransformer
-
 from qdrant_client import QdrantClient
-from qdrant_client.http.models import Distance, VectorParams
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.llms import HuggingFaceHub
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Qdrant
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
 
-from langchain.llms import OpenAI
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-logger = logging.getLogger(__name__)
+embeddings = OpenAIEmbeddings()
+collection_name = "resume_test"
 client = QdrantClient("localhost", port=6333)
-collection_name = "resumes_test"
+qdrant = Qdrant(client, collection_name, embeddings)
 
+url = "localhost:6333"
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def welcome():
-    welcome = "Welcome to PDF analyzer"
-    return welcome.strip('"')
+pages = ()
 
-@app.get("/hello")
-def hello(name: Annotated[str, Query()] = "Wilson"):
-    return {"message": f"Hello {name}!"}
-
-@app.get("/processPDF")
-def main():
-    print("Starting the process...")
-    pages_data = ()
-    documents = []
-    vectorEmbeddings = []
-    qdrant_ref = ()
+# @app.get("/processPDF")
+# def main():
+#     print("Starting the process...")
+#     pdf_data = ()
    
-    #attempt to split the pages and extract the data
-    try:
-        pages_data = split_text_by_pages()
-        print("split_text_by_pages completed")
-    except:
-        print("split_text_by_pages error")
+#     #Step 1: load PDF into array of documents
+#     try:
+#         pdf_data = load_pdf()
+#         print("pdf loaded")
+#     except:
+#         print("error step 1: load pdf")
     
-    #attempt to organize data 
-    try:
-        documents = organize_data_into_documents(pages_data)
-        print("organized documents data")
-    except:
-        print("organize_data_into_documents error")
+#     #Step 2: load data into qdrant 
+#     try:
+#         load_qdrant(pdf_data)
+#         print("data loaded into qdrant")
+#     except Exception as e:
+#         logging.error("Exception occurred", exc_info=True)
+#         print("error step 2: load data into qdrant")
 
-    # #attempt to chunk the data
-    # try:
-    #     documents = organize_data_into_chunks(documents)
-    #     print("organized data into chunks")
-    # except:
-    #     print("organize_data_into_chunks error")
+@app.get("/query_resume")
+async def query_qdrant(question: str = Query(..., description="A question about Ernest's resume")):
     
-    #attempt to generate vector embeddings from text
+    pdf_data = []
+   
+    #Step 1: load PDF into array of documents
     try:
-        vectorEmbeddings = create_vector_embeddings(documents)
-        print("documents transformed into vector embeddings")
-    except: 
-        print("transforming documents into vector embedding failed")
+        pdf_data = load_pdf()
+        # pdf_data = minimal_load_pdf()
+        print("pdf loaded", pdf_data)
+    except:
+        print("error step 1: load pdf")
     
-    #attempt to initialize qdrant collection #attempt to initialize qdrant collection
+    #Step 2: load data into qdrant 
     try:
-        initialize_response = client.recreate_collection(
-            #  switch from client.recreate to client.create_collection in production
-                collection_name= collection_name,
-                vectors_config=VectorParams(size=384, distance=Distance.DOT),
-            )
-        if initialize_response != "True":
-            #continue
-            print("Collection already initialized, continue...")
-        else: 
-            print("New collection initialized")
-    except:
-        print("error intializing qdrant collection")
+        print("data to be loaded into qdrant:", pdf_data)
+        load_qdrant(pdf_data)
+        print("data loaded into qdrant")
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
+        print("error step 2: load data into qdrant")
 
+    #Step 3. ask question
     try:
-        qdrant_ref = store_data_in_qdrant(vectorEmbeddings, documents)
-        print("store_data_in_qdrant completed")
-        print("qdrant:", qdrant_ref)
-    except:
-        print("store_data_in_qdrant error")
+        response_answer = get_answer(question, pdf_data)
+    except Exception as e:
+        logging.error("Exception occurred", exc_info=True)
+        print("error step 3: ask question")
 
-def split_text_by_pages():
+def load_pdf():
     # Open the PDF file
-    doc = fitz.open("/Users/jason/repos/se-challenge-codeslip/PDFs/Ernest_Hemingway_Resume.pdf")
-    #Initialize arrays to store data from each page
-    pages_text = []  
-    pages_number = [] 
-    pages_title = [] 
-    # Iterate over each page in the document
-    for page_number, page in enumerate(doc, start=1):
-        # Extract text from the current page
-        text = page.get_text()
-        # Extract the page title
-        lines = text.split("\n")  # Split the text into lines
-        title = "Ernest Hemingway Resume"
-        page_title = ()
-        for i, line in enumerate(lines):
-            if title in line:
-                # Ensure there is a next line to return
-                if i + 1 < len(lines):
-                    page_title = lines[i + 1]
-                else:
-                    page_title = ""  # Return an empty string if the title is the last line
-        pages_text.append(lines)
-        pages_number.append(page_number)
-        pages_title.append(page_title)
-    # Close the document
-    doc.close()
+    loader = PyPDFLoader("/Users/jason/repos/se-challenge-codeslip/PDFs/Ernest_Hemingway_Resume.pdf")
+    pages = loader.load_and_split()
+    text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=20)
+    docs = text_splitter.split_documents(pages)
+    for doc in docs:
+            # Assuming 'doc' is a dictionary and 'page_content' is a key
+            # Convert 'page_content' to string if it's not already
+            if 'page_content' in doc and not isinstance(doc['page_content'], str):
+                print("page_content in the correct location")
+                doc['page_content'] = str(doc['page_content'])
+    return docs
 
-    return pages_text, pages_number, pages_title
-
-def organize_data_into_documents(pages_data):
-    pages_text, pages_numbers, pages_titles = pages_data
-    
-    documents = []  # Initialize the list to store the document dictionaries
-
-    # Iterate through the raw pages and their corresponding titles and numbers
-    for index, content in enumerate(pages_text):
-        page_number = str(pages_numbers[index])  # Convert page number to string
-        page_title = pages_titles[index]
-        
-        # Join the text content, excluding the first item (which is a repeated title)
-        page_text = " ".join(content[1:])  # Join the text parts into a single string
-        
-        # Create a dictionary for the current page and append it to the documents list
-        documents.append({
-            "page_number": page_number,
-            "page_title": page_title,
-            "page_text": page_text
-        })
-    
-    return documents
-
-# def organize_data_into_chunks(documents):
-#     print("Create chunks")
-#     for document in documents:
-#         l= document["page_text"].split('.')
-#         res= {}
-#         for i in range(len(l)//20+1):
-#             res[i]=l[i:i+20]
-#         print("chunked: ",res)
-
-def create_vector_embeddings(documents):
-    print("starting embedding:", documents)
-    vectors = []
-    vectors = model.encode(
-        [document["page_title"] + ". " + document["page_text"] for document in documents]
-    )
-    print("vectors:", vectors)
-    return vectors
-
-def store_data_in_qdrant(vectorEmbeddings, documents): 
-    #beginning storing process
-    uploadStatus = client.upload_collection(
+def load_qdrant(pdf_data):
+    qdrant_load = qdrant.from_documents(
+        pdf_data,
+        embeddings,
+        url=url,
         collection_name= collection_name,
-        vectors=vectorEmbeddings,
-        payload=documents,
-        ids=None,  # Vector ids will be assigned automatically
-        batch_size=512,  # How many vectors will be uploaded in a single request?
+        content_payload_key= collection_name,
+        metadata_payload_key= collection_name
     )
-    return uploadStatus
+    print("qdrant loaded: ", qdrant_load)
 
-@app.get("/question-answer")
-async def query_similar_documents(question: str = Query(..., description="The natural language question")):
-    # Generate an embedding for the question
-    custom_prompt_template = PromptTemplate(
-        template= question, input_variables=["context", "question"]
-    )
-    print("test!")
-    custom_qa = VectorDBQA.from_chain_type(
-        llm=llm, 
-        chain_type="stuff", 
-        vectorstore=qdrant_store,
-        return_source_documents=False,
-        chain_type_kwargs={"prompt": custom_prompt_template},
+def get_answer(question, pdf_data):
+    qdrant_get = qdrant.from_documents(
+        pdf_data,
+        embeddings,
+        url=url,
+        collection_name= collection_name,
+        content_payload_key= collection_name,
+        metadata_payload_key= collection_name
     )
 
-    print("test 2!", custom_qa)
+    print("Question:", question)
+    answer = qdrant_get.similarity_search(question)
+    print(answer[0].page_content)
+  
+    # return answer 
+
+    # # Convert text query into vector
+    # vector = qdrant.encode(question).tolist()
+
+    # # Use `vector` for search for closest vectors in the collection
+    # search_result = qdrant.qdrant_client.search(
+    #     collection_name= collection_name,
+    #     query_vector=vector,
+    #     query_filter=None,  # If you don't want any filters for now
+    #     limit=5,  # 5 the most closest results is enough
+    # )
     
-    return {"question": question, "answer": custom_qa}
+    # payloads = [hit.payload for hit in search_result]
+    # print("payloads:",payloads)
+    # return payloads
+    
